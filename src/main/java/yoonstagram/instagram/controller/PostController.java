4bbb3f8955f2220661a35a45fb6aa9185afb06cf
yooniversal <iteratively@naver.com>
2023-02-06 04:38:08 +0900

    검색 화면에서 프로필 이미지 출력 안되는 문제 수정

--- src/main/java/yoonstagram/instagram/controller/PostController.java
+++ src/main/java/yoonstagram/instagram/controller/PostController.java
@@ -1,14 +1,10 @@
 package yoonstagram.instagram.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import yoonstagram.instagram.config.auth.PrincipalDetails;
 import yoonstagram.instagram.domain.Comment;
@@ -20,6 +16,7 @@ import yoonstagram.instagram.domain.dto.SimpleUserDto;
 import yoonstagram.instagram.domain.dto.UploadPostDto;
 import yoonstagram.instagram.service.CommentService;
 import yoonstagram.instagram.service.PostService;
+import yoonstagram.instagram.service.UserService;
 
 import java.util.List;
 
@@ -29,6 +26,7 @@ public class PostController {
 
     private final PostService postService;
     private final CommentService commentService;
+    private final UserService userService;
 
     @GetMapping("/post/upload")
     public String upload(Model model,
@@ -89,7 +87,9 @@ public class PostController {
     public String searchTag(@PathVariable("tag") String tag,
                             Model model,
                             @AuthenticationPrincipal PrincipalDetails principalDetails) {
-        User currentUser = principalDetails.getUser();
+        Long currentUserId = principalDetails.getUser().getId();
+        User currentUser = userService.findOneById(currentUserId);
+
         model.addAttribute("currentUserId", currentUser.getId());
         model.addAttribute("currentUserImageUrl", currentUser.getImageUrl());
 
